1ebd9a5c631cbb747dec2d551471e518ba0ebb84
Jesse Reich <jrreich@gmail.com>
2017-01-07 20:00:54 -0500

    got a lot more working MEO input analysis (excel and MS SQL db)

--- routes.py
+++ routes.py
@@ -3,7 +3,9 @@ from flask import Flask, url_for, request, render_template;
 from app import app
 import beacon_decode as bcn
 from werkzeug.utils import secure_filename
-
+import pypyodbc
+import MEOInput_Analysis
+import datetime
 
 UPLOAD_FOLDER = 'var/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','db','zip'])
@@ -57,6 +59,47 @@ def rawburst():
             MEOLUT = result['MEOLUT'], \
             StartTime = result['StartTime'], \
             file_name = secure_filename(f.filename))
+
+
+    else: 
+        return '<h2> Invalid Request </h2>'
+@app.route('/MEOInputAnalysis', methods=['GET','POST'])
+def MEOInputAnalysis():
+    if request.method == 'GET':
+        # Send the user the form
+        return render_template('MEOInputAnalysisForm.html')
+    elif request.method == 'POST':
+        # read input
+        result = request.form
+        #print result['StartTime']
+        MEOLUT = int(result['MEOLUT'])
+        StartTime = datetime.datetime.strptime(result['StartTime'],'%Y-%m-%dT%H:%M')
+        EndTime = datetime.datetime.strptime(result['EndTime'],'%Y-%m-%dT%H:%M')
+        print StartTime
+        if result['inputsource'] in ["excelfile", "zipfile", "sqldbfile"]:
+            f = request.files['inputfile'] 
+            filesaved = UPLOAD_FOLDER + '/' + secure_filename(f.filename)    
+            f.save(filesaved)
+            if result['inputsource'] == 'excelfile':
+                MEOInput_Analysis.xlx_analysis(UPLOAD_FOLDER, secure_filename(f.filename), MEOLUT, StartTime, EndTime, result['beaconID'])
+        else:
+            conn = pypyodbc.connect(r'Driver={SQL Server};Server=.\SQLEXPRESS;Database=mccoperational;Trusted_Connection=yes;')
+            cursor = conn.cursor()
+            print result['MEOLUT'], result['StartTime'][:10], result['EndTime'][:10]
+            cursor.execute("Select * from MEOInputSolution where SourceId = ? and TimeSolutionGenerated BETWEEN ? AND ?", [result['MEOLUT'],result['StartTime'][:10], result['EndTime'][:10]])
+            for dataRow in cursor.fetchall():
+                print(dataRow)
+            #    crsr.execute(sql, dataRow)
+            cursor.close()
+            conn.close()
+            
+        print result
+        print result['StartTime']
+        print result.getlist('MEOLUT')  #.encode('ascii','ignore') - need to do it to each element not list
+        return render_template('MEOInputAnalysisReturn.html', \
+            result = result)
+            #StartTime = result['StartTime'], \
+            #file_name = secure_filename(f.filename))
 
 
     else: 
