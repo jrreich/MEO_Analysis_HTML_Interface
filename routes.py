f63a8fbbe5665ede884a72413e3c1de616eb753b
jrreich <jrreich@gmail.com>
2018-05-15 23:15:14 -0400

    push attempt3

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from werkzeug.utils import secure_filename
-from flask import Flask, url_for, request, render_template; 
+from flask import Flask, url_for, request, render_template, jsonify; 
 #from app import app
 import beacon_decode as bcn
 from werkzeug.utils import secure_filename
@@ -9,22 +9,34 @@ import datetime
 import csv
 import sys
 import os
+#import sendsms as sms
 
 UPLOAD_FOLDER = os.path.join('var','uploads')
+
 approot = os.path.dirname(__file__)
+
 OUTPUTFOLDER = os.path.join('static','output')
 #OUTPUTFOLDER = r'C:/Users/reichj/Source/Repos/MEO_Analysis_HTML_Interface/static/output/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','db','zip'])
 
-#servername = 'localhost' #for deploying on MCC
-servername = r'.\SQLEXPRESS' #for deploying on REICHJ-PC
-#oppsdatabase = 'mccoperationalRpt' #for deploying on MCC
-#oppsdatabase = 'mccoperational' # for deploying on REICHJ-PC
-oppsdatabase = 'MccMeoLutMonitor' #for deploying on MCC - new db																
+Deploy_on = 'other1' # for deploying on RYZEN7 2018-02-21
+#Deploy_on = 'MCC'	
+#Deploy_on = 'other2'
 
-#mcctestLGM = 'MccTestLGM' #should work for both MCC and REICHJ-PC
-mcctestLGM = 'MccMeoLutMonitor' #for deploying on MCC - new db															  
-
+if Deploy_on == 'MCC':
+    servername = 'localhost' #for deploying on MCC
+    #oppsdatabase = 'mccoperationalRpt' #for deploying on operational MCC
+    oppsdatabase = 'MccMeoLutMonitor' #for deploying on MCC - new db
+    mcctestLGM = 'MccMeoLutMonitor' #for deploying on MCC - new db	
+elif Deploy_on == 'other1':
+    servername = r'.\SQLEXPRESS' #for deploying on REICHJ-PC - 2018 and on
+    oppsdatabase = 'MccMeoLutMonitor' # for deploying on REICHJ-PC
+    mcctestLGM = 'MccMeoLutMonitor' #should work for both MCC and REICHJ-PC
+else: 
+    servername = r'.\SQLEXPRESS' #for deploying on REICHJ-PC
+    oppsdatabase = 'mccoperational' # for deploying on REICHJ-PC
+    mcctestLGM = 'MccTestLGM' #should work for both MCC and REICHJ-PC
+														
 app = Flask(__name__)
 
 def allowed_file(filename):
@@ -32,9 +44,9 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #server/Main
-@app.route('/')
+@app.route('/', methods =['GET'])
 def index():
-    """Renders a sample page."""
+    """Renders home page."""
     #createLink = "<a href = '" + url_for('beacon') + "'>Beacon Decoder</a>"; # url_for usings the function name to point to URL
     return render_template('index3.html')
 
@@ -93,6 +105,40 @@ def rawburst():
             return render_template('BurstAnalysisReturn.html', filelist=filelist )
     else: 
         return '<h2> Invalid Request </h2>'
+@app.route('/MEOData', methods=['GET','POST'])
+def meodata():
+    if request.method == 'GET':
+        # Send the user the form
+        return render_template('MeoDataCollection.html')
+    elif request.method == 'POST':
+        result = request.form
+        if result.get('beaconID', False):
+            beaconId = result['beaconID']
+        else:
+            beaconId = '%'
+        if result.get('EndTime', False):
+            EndTime = datetime.datetime.strptime(result['EndTime'],'%Y-%m-%dT%H:%M')
+        else:
+            EndTime = datetime.datetime.utcnow()
+        if result.get('StartTime', False):
+            StartTime = datetime.datetime.strptime(result['StartTime'],'%Y-%m-%dT%H:%M')
+        else:
+            StartTime = EndTime - datetime.timedelta(hours = float(168))
+        if 'MEOLUT' in result: 
+            MEOLUTList = [int(x) for x in result.getlist('MEOLUT')]
+        else:
+            MEOLUTList = ['%']
+        filelist = MEOInput_Analysis.api_MeoDataCollection(beaconId, MEOLUTList, StartTime, EndTime, OUTPUTFOLDER, approot, servername, mcctestLGM) 
+        return render_template('MeoDataCollection.html', linklist = filelist)
+        #if result['inputsource'] in ["excelfile", "zipfile", "sqldbfile"]:
+        #    f = request.files['inputfile'] 
+        #    filesaved = os.path.join(approot, UPLOAD_FOLDER,secure_filename(f.filename))
+        #    f.save(filesaved)
+        #elif result['inputsource'] == 'mccdb':
+        #    filelist = MEOInput_Analysis.MeoDataCollection(result, MEOLUTList, StartTime, EndTime, OUTPUTFOLDER, approot, servername, mcctestLGM) #,sql_login = 'yes') # sql_login uses FreeTDS and sql login rather than windows auth - used for linux
+        #    return render_template('BurstAnalysisReturn.html', filelist=filelist )
+    else: 
+        return '<h2> Invalid Request </h2>'
 
 @app.route('/MEOInputAnalysis', methods=['GET','POST'])
 def MEOInputAnalysis():
@@ -102,19 +148,15 @@ def MEOInputAnalysis():
     elif request.method == 'POST':
         # read input
         result = request.form
-        MEOLUT = int(result['MEOLUT'])
-        if result['StartTime']:
+        if result.get('RealPastTime',False) == 'RT_yes':
+            EndTime = datetime.datetime.utcnow()
+            StartTime = EndTime - datetime.timedelta(hours = float(result['realtimehours']))
+        elif result.get('StartTime',False) <> '':
             StartTime = datetime.datetime.strptime(result['StartTime'],'%Y-%m-%dT%H:%M')
-        else:
-            StartTime = datetime.datetime(2015,1,1,0,0,0)
-        if result['EndTime']:
             EndTime = datetime.datetime.strptime(result['EndTime'],'%Y-%m-%dT%H:%M')
         else:
             EndTime = datetime.datetime.utcnow()
-            if result['realtimehours']:
-			    StartTime = EndTime - datetime.timedelta(hours = float(result['realtimehours']))
-            else:
-				StartTime = EndTime - datetime.timedelta(hours = 24)								   													
+            StartTime = EndTime - datetime.timedelta(days = 7)
         if result['inputsource'] in ["excelfile", "zipfile", "sqldbfile"]:
             f = request.files['inputfile'] 
             filesaved = os.path.join(approot, UPLOAD_FOLDER,secure_filename(f.filename))
@@ -122,7 +164,7 @@ def MEOInputAnalysis():
             if result['inputsource'] == 'excelfile':
                 MEOInput_Analysis.xlx_analysis(filesaved, OUTPUTFOLDER, MEOLUT, StartTime, EndTime, result) # need to add approot if this will be functional on apache
         elif result['inputsource'] == 'mccdb':
-            csvoutfile, filelist = MEOInput_Analysis.MSSQL_analysis(result, MEOLUT, StartTime, EndTime, OUTPUTFOLDER, approot, servername, oppsdatabase) 
+            csvoutfile, filelist = MEOInput_Analysis.MSSQL_analysis(result, StartTime, EndTime, OUTPUTFOLDER, approot, servername, oppsdatabase) 
             if csvoutfile == None:
                 return render_template('MEOInputAnalysisReturnNone.html', data = filelist)
             rdr= csv.reader( open(os.path.join(approot,csvoutfile), "r" ))
@@ -180,14 +222,15 @@ def opensites():
         if request.args.get('sitenum') is not None:
             RCC_KML = False
             Input_KML = False
-            bothfile = False
+            KMLfile = False
+            Mapfile = False
             sitenum = request.args.get('sitenum')
             if request.args.get('RCC_KML') is not None:
                 RCC_KML = request.args.get('RCC_KML')        
             if request.args.get('Input_KML') is not None:
                 Input_KML = request.args.get('Input_KML')  
             if request.args.get('Both_KML') is not None:
-                bothfile = MEOInput_Analysis.both_kml(sitenum,OUTPUTFOLDER,approot,servername,oppsdatabase)
+                KMLfile, Mapfile = MEOInput_Analysis.both_kml(sitenum,OUTPUTFOLDER,approot,servername,oppsdatabase)
             
             alertsitesum = MEOInput_Analysis.alertsitesum_query(sitenum,OUTPUTFOLDER,approot,servername,oppsdatabase)
             alertsitesols, Input_KMLfile = MEOInput_Analysis.alertsitesol_query(sitenum,OUTPUTFOLDER,approot,servername,oppsdatabase, makeKML = Input_KML)
@@ -202,7 +245,8 @@ def opensites():
                 outsitesols = outsitesols,
                 RCC_KMLfile = RCC_KMLfile,
                 Input_KMLfile = Input_KMLfile,
-                Both_KMLfile = bothfile,
+                KMLfile = KMLfile,
+                Mapfile = Mapfile
                 )
         else:
             open_site_list = MEOInput_Analysis.Open_Sites(servername,oppsdatabase)  # list of tuples
@@ -218,28 +262,127 @@ def MEOBeaconAnalysis():
     elif request.method == 'POST':
         # read input
         result = request.form
-        MEOLUT = int(result['MEOLUT'])
-        if result['StartTime']:
-            StartTime = datetime.datetime.strptime(result['StartTime'],'%Y-%m-%dT%H:%M')
-        else:
-            StartTime = datetime.datetime(2015,1,1,0,0,0)
-        if result['EndTime']:
-            EndTime = datetime.datetime.strptime(result['EndTime'],'%Y-%m-%dT%H:%M')
-        else:
+        print result
+        if result.get('RealPastTime',False) == 'RT_yes':
+            print 'should be here'
             EndTime = datetime.datetime.utcnow()
-        if result['inputsource'] in ["excelfile", "zipfile", "sqldbfile"]:
-            f = request.files['inputfile'] 
+            StartTime = EndTime - datetime.timedelta(hours = float(168))
+            print 'check again'
+        else:
+            StartTime = datetime.datetime.strptime(result['StartTime'],'%Y-%m-%dT%H:%M')
+            EndTime = datetime.datetime.strptime(result['EndTime'],'%Y-%m-%dT%H:%M')	
+        filesaved = False
+        print 'still here?'
+        if result['GTSource'] == 'GTFile':
+            print 'using file'
+            f = request.files['gt_inputfile'] 
             filesaved = os.path.join(approot, UPLOAD_FOLDER,secure_filename(f.filename))
             f.save(filesaved)
-            if result['inputsource'] == 'excelfile':
-                MEOInput_Analysis.xlx_analysis(filesaved, OUTPUTFOLDER, MEOLUT, StartTime, EndTime, result) # need to add approot if this will be functional on apache
-        elif result['inputsource'] == 'mccdb':
-            csvoutfile, filelist = MEOInput_Analysis.MSSQL_analysis(result, MEOLUT, StartTime, EndTime, OUTPUTFOLDER, approot, servername, oppsdatabase) 
-            if csvoutfile == None:
-                return render_template('MEOInputAnalysisReturnNone.html', data = filelist)
-            rdr= csv.reader( open(os.path.join(approot,csvoutfile), "r" ))
-            csv_data = [ row for row in rdr ]
-            return render_template('MEOInputAnalysisReturn.html', data=csv_data, linklist = filelist)
+        print filesaved
+        csvoutfile, imglist, filelist = MEOInput_Analysis.MSSQL_beacon_analysis(result, StartTime, EndTime, OUTPUTFOLDER, approot, servername, oppsdatabase, filesaved) 
+        if csvoutfile == None:
+            print 'csvoutfile was None'
+            return render_template('MEOBeaconAnalysisReturnNoData.html', result = result, StartTime = StartTime, EndTime = EndTime)
+        rdr= csv.reader( open(os.path.join(approot,csvoutfile), "r" ))
+        csv_data = [ row for row in rdr ]
+        return render_template('MEOBeaconAnalysisReturn.html', data=csv_data, imglist = imglist, linklist = filelist)
 
+    else: 
+        return '<h2> Invalid Request </h2>'
+@app.route('/MapTest', methods=['GET','POST'])
+def MapTest():
+    if request.method == 'GET':
+        if request.args.get('KML') is not None:
+            return render_template('MapTest_v3.html', KMLFILE = request.args.get('KML'))
+        else: 
+            return render_template('MapTest_v3.html')
+
+
+@app.route('/api/sitesum/<int:sitenum>', methods=['GET','POST'])
+def sitereturn(sitenum):
+    print sitenum
+    if request.method == 'GET':
+        SiteData = MEOInput_Analysis.api_site_sum_query(sitenum,servername, oppsdatabase)
+        try: 
+            data = SiteData
+            print 'got data'
+            print data
+        except IndexError:
+            abort(404)
+        print jsonify(data)
+        return jsonify(data)
+
+@app.route('/api/J1', methods = ['POST'])
+def J1_json():
+    data = request.args.to_dict()
+    print data
+
+@app.route("/stream")
+def stream():
+    AllSiteData = MEOInput_Analysis.czml_all_sites_sum_query(servername, oppsdatabase)
+    return jsonify(AllSiteData)
+    def eventStream():
+        while True:
+            # Poll data from the database
+            # and see if there's a new message
+            if len(messages) > len(previous_messages):
+                yield "data: {}\n\n".format(messages[len(messages)-1])
+    
+    return Response(eventStream(), mimetype="text/event-stream")
+
+@app.route("/api/czml/meo/per/<int:sourceid>")
+def czml_meolut_ant_per(sourceid):
+    currentDateTime = datetime.datetime.utcnow()
+    bcnid = 'ADDC00202020201'
+    MeoPercent = MEOInput_Analysis.czml_meolut_ant_per(currentDateTime, bcnid, sourceid, servername, mcctestLGM)
+    return MeoPercent 
+
+@app.route("/api/czml/site/<int:sitenum>")
+def czml_meo_input_by_site(sitenum):
+    MeoInput = MEOInput_Analysis.czml_site_meo_input(sitenum, servername, oppsdatabase)
+    return jsonify(MeoInput)
+
+@app.route("/api/czml/orbit/meo")
+def czml_meo_sat_orbit():
+    #starttime = datetime.datetime(2017,8,21)
+    #endtime = datetime.datetime(2017,8, 22)
+    endtime = datetime.datetime.utcnow() + datetime.timedelta(days = 1)
+    starttime = endtime - datetime.timedelta(days = 2)
+    OrbitData = MEOInput_Analysis.czml_meo_orbit_all(starttime, endtime, servername, oppsdatabase ) #, satnum)
+    return jsonify(OrbitData)
+
+@app.route("/api/czml/orbit/leo")
+def czml_leo_sat_orbit():
+    #starttime = datetime.datetime(2017,8,19)
+    #endtime = datetime.datetime(2017,8, 22)
+    endtime = datetime.datetime.utcnow() + datetime.timedelta(days = 1)
+    starttime = endtime - datetime.timedelta(days = 1)
+    OrbitData = MEOInput_Analysis.czml_leo_orbit_all(starttime, endtime, servername, oppsdatabase ) #, satnum)
+    return jsonify(OrbitData)
+
+@app.route("/api/czml/sched/meo")
+def czml_meo_schedule_all():
+    starttime = datetime.datetime(2017,8,19)
+    endtime = datetime.datetime(2017,8, 22)
+    #endtime = datetime.datetime.utcnow() + datetime.timedelta(days = 1)
+    #starttime = endtime - datetime.timedelta(days = 1)
+    MeoSchedData = MEOInput_Analysis.czml_meo_sched_all(starttime, endtime, servername, oppsdatabase ) #, antenna)
+    return jsonify(MeoSchedData)
+
+@app.route('/messager', methods=['GET','POST'])
+def messager():
+    if request.method == 'GET':
+        # Send the user the form
+        return render_template('messager.html')
+    elif request.method == 'POST':
+        # read beacon ID and save it
+        phonenumber = request.form['phonenumber']        
+        messagetext = request.form['messagetext']
+        #decode and return it
+        #return results
+        sms.send_sms(phonenumber, messagetext)
+        return render_template('messagesent.html', \
+            phonenumber = phonenumber, \
+            messagetext = messagetext)
     else: 
         return '<h2> Invalid Request </h2>'
---
+++ static/scripts/NOAAczml.js
@@ -0,0 +1,146 @@
+var app = (function () {
+    function addCzmlDataSource(data) {
+            var czmlDataSource = Cesium.CzmlDataSource.load(data);
+            viewer.dataSources.add(czmlDataSource);
+        };
+    var LUTsczml =
+        [{"id" : "document",
+        "version" : "1.0"
+        },
+        {"id" : "NSOF",
+        "name" : "NSOF",
+        "label": {
+            "text":"NSOF",
+            "horizontalOrigin": "CENTER",
+            "pixelOffset": {
+                "cartesian2" : [0, -20]
+            },
+            "fillColor": {
+                "rgba": [63, 191, 191, 255]
+            }
+        },
+        "position": {
+            "cartographicDegrees":[-76.93677,38.850913,17]
+        },
+        "description":"NOAA Satellite Operations Facility",
+        "billboard":{
+            "horizontalOrigin":"CENTER",
+            "image":"/static/icons/NOAA_svg.png",
+            "scale":0.06,
+            "outlineWidth": 2,
+            "outlineColor": {
+                "rgba":[255, 255, 255, 255] 
+            },           
+            "pixelOffset": {
+                "cartesian2": [ 0, 10 ]
+            }
+        }/* ,
+        "point": {
+            "color": {
+                "rgba":[63, 191, 191, 255]
+            },
+            "pixelSize" : 20
+        }*/
+        },
+        {"id" : "FL",
+        "name" : "FL",
+        "label": {
+            "text":"FLLUTs",
+            "horizontalOrigin": "CENTER",
+            "pixelOffset": {
+                "cartesian2" : [0, -20]
+            }
+        },
+        "description":"Florida LEO/MEOLUTs",
+        "position":{"cartographicDegrees":[-80.383274,25.617562,0]},
+        "point": {
+            "color": { "rgba": [0,0,255,255] },
+            "pixelSize" : 20
+        }
+        },
+        {"id" : "HI",
+        "label":{"text":"HILUTs"},
+            "description":"Hawaii LEO/MEOLUTs",
+            "position":{"cartographicDegrees":[-158.001297,21.524410,0]},
+        },
+        {"id" : "AK",
+        "label":{"text":"FCDAS"},
+            "description":"Fairbanks Command and Data Acquisition Station",
+            "position":{"cartographicDegrees":[-147.515622,64.973725,0]},
+        },
+        {"id" : "GU",
+        "label":{"text":"GULUTs"},
+            "description":"Guam LEO LUTs",
+            "position":{"cartographicDegrees":[144.939074,13.578298,0]},
+        }];
+
+    var czmlSource = new Cesium.CzmlDataSource();
+    viewer.dataSources.add(czmlSource);
+    czmlSource.load(LUTsczml);
+
+    var czmlStreamStartSource = new Cesium.CzmlDataSource();
+    viewer.dataSources.add(czmlStreamStartSource);
+    czmlStreamStartSource.load('/stream')
+
+    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+
+    var handler2 = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+    handler2.setInputAction(function(click) {
+            event.preventDefault();//OR click.preventDefault();
+            var pickedObject = viewer.scene.pick(click.position);
+            if (Cesium.defined(pickedObject)) {
+                    var czmlSite = new Cesium.CzmlDataSource();
+                    viewer.dataSources.add(czmlSite);
+                    czmlSite.load('/api/czml/site/' + pickedObject.id._id).then(function() {
+                        viewer.flyTo(czmlSite, {
+                            duration: 5,
+                            offset: new Cesium.HeadingPitchRange(0, -Math.PI / 4, 150000)
+                        });
+                    });
+                    if(pickedObject.id._name) {
+                            $('.cesium-viewer-infoBoxContainer').show();
+                            flyTo(pickedObject.id._id);
+                            console.log('showing something in the info box')
+                    }
+            }
+    }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+
+
+    var czmlSat = new Cesium.CzmlDataSource();
+    viewer.dataSources.add(czmlSat);
+    var centerAOR = viewer.entities.add({
+        id : 'USAOR_Center',
+        name : 'USAOR_Center',
+        label : 'center',
+        position : Cesium.Cartesian3.fromDegrees(-118,34),
+        point : {
+            show : 'False'
+        }
+
+    });
+    //czmlSat.load('/api/czml/orbit/meo').then(function() {
+    //    viewer.zoomTo(czmlSat.entities._entities._array[0])
+    //});
+    viewer.flyTo(centerAOR, 
+        {offset : new Cesium.HeadingPitchRange(0, (-Math.PI / 2), 20000000)}
+    );
+    /*
+    viewer.flyTo(czmlSource.entities.getById('NSOF'), //This works 
+        {offset : new Cesium.HeadingPitchRange(0, (-Math.PI / 2), 20000000)}
+    */ 
+
+
+    /*
+    var clock1 = new Cesium.Clock({
+        multiplier: 1
+    });
+    viewer.clock = clock1
+    */
+
+    /*
+    var czmlLeoSat = new Cesium.CzmlDataSource();
+    viewer.dataSources.add(czmlLeoSat);
+    czmlLeoSat.load('/api/czml/orbit/leo');
+    console.log('leos loaded')
+    */
+})();
