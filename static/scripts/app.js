67d4c4cafcf9c33d0443c61bb6dcdfef50380fc9
jrreich <jrreich@gmail.com>
2018-09-11 01:06:55 -0400

    added awesome toolbar

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -8,6 +8,47 @@ var app = (function () {
                 credit: 'testing credits'
             })
         });
+
+        var cartographic = new Cesium.Cartographic();
+        var cartesian = new Cesium.Cartesian3();
+        var camera = viewer.scene.camera;
+        var ellipsoid = viewer.scene.mapProjection.ellipsoid;
+        var toolbar = document.getElementById('toolbar');
+        toolbar.innerHTML = '<div id="hud"></div>' +
+            '<button type="button" class="cesium-button" id="h1km">1km height</button>' +
+            '<button type="button" class="cesium-button" id="h10km">10km height</button>' +
+            '<button type="button" class="cesium-button" id="h500km">500km height</button>';
+
+        toolbar.setAttribute('style', 'background: rgba(42,42,42,0.9); border-radius: 5px;');
+
+        var hud = document.getElementById('hud');
+
+        viewer.clock.onTick.addEventListener(function (clock) {
+            ellipsoid.cartesianToCartographic(camera.positionWC, cartographic);
+            hud.innerHTML =
+                'Lon: ' + Cesium.Math.toDegrees(cartographic.longitude).toFixed(3) + ' deg<br/>' +
+                'Lat: ' + Cesium.Math.toDegrees(cartographic.latitude).toFixed(3) + ' deg<br/>' +
+                'Alt: ' + (cartographic.height * 0.001).toFixed(1) + ' km';
+        });
+
+        function setHeightKm(heightInKilometers) {
+            ellipsoid.cartesianToCartographic(camera.position, cartographic);
+            cartographic.height = heightInKilometers * 1000;  // convert to meters
+            ellipsoid.cartographicToCartesian(cartographic, cartesian);
+            camera.position = cartesian;
+        }
+
+        document.getElementById('h1km').addEventListener('click', function () {
+            setHeightKm(1);
+        }, false);
+
+        document.getElementById('h10km').addEventListener('click', function () {
+            setHeightKm(10);
+        }, false);
+
+        document.getElementById('h500km').addEventListener('click', function () {
+            setHeightKm(500);
+        }, false);
         //viewer.extend(Cesium.viewerDragDropMixin);
         czmlDataSource = new Cesium.CzmlDataSource();
         viewer.dataSources.add(czmlDataSource);
